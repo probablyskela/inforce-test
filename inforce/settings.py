9edd2650b209931c53393020a29904c6830fd75a
Oleksandr Yovbak <oleksandr.yovbak@gmail.com>
2023-08-12 13:09:53 +0300

    Add flake8

--- inforce/settings.py
+++ inforce/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG") == True
+DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = ["0.0.0.0"]
 
