1cfdc5b5267a6e064872989e650ac9ba93c2589e
Oleksandr Yovbak <oleksandr.yovbak@gmail.com>
2023-08-13 04:38:27 +0300

    Create restaurants app

--- inforce/urls.py
+++ inforce/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),   
-    path('employees/', include('employees.urls'))
+    path("admin/", admin.site.urls),
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("employees/", include("employees.urls")),
+    path("", include("restaurants.urls")),
 ]
